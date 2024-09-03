ddf88a93b7a82657950126f9712be893d6df890e
Nyakallo1996 <Lekgoabamahlaks@gmail.com>
2024-09-03 22:18:25 +0200

    small changes

--- Chapter_9/src/ex_09_02/ShoppingCart.java
+++ Chapter_9/src/ex_09_02/ShoppingCart.java
@@ -6,9 +6,11 @@ public class ShoppingCart {
     public static void main(String args[]){
 
         // Declare, instantiate, and initialize a Customer object
-        Customer cust1 = new Customer("Bob Miller", "555-44-3212");
+        Customer cust1 = new Customer("Hlapi Mamabolo", "555-44-3212");
+        Customer cust2 = new Customer("Nyakallo Mahlakametsa", "555-66-3232");
 
 	// Print the customer object name
         System.out.println("Customer name: "+cust1.getName());
+        System.out.println("Customer name: "+cust2.getName());
     }    
 }
